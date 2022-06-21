689675f4797d2c93e318fa96f6e463d39c1a00b5
Chinaza  Okeke <104084551+ChinazaOkeke@users.noreply.github.com>
2022-06-21 00:25:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML = "Name: Chinaza Okeke";
+document.getElementById("dem").innerHTML = "Height: 5 feet 3 inches";
+document.getElementById("de").innerHTML = "Country: Nigeria";
